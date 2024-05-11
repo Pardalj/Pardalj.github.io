3b1651f237b6a52839297ecb23cb87bd7b63ca62
Pardal <141125423+Pardalj@users.noreply.github.com>
2024-05-11 21:34:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -88,6 +88,6 @@ let usercard = document.getElementById('usercard');
 
 let p = document.createElement('p');
 
-p.innerText = `${tg.initData.user.first_name} ${tg.initData.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.first_name} ${tg.initDataUnsafe.last_name}`;
 
 usercard.appendChild(p);
