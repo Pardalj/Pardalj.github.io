305ac60e87a7fda67c0cf5e0f916e6d7d706f9a7
Pardal <141125423+Pardalj@users.noreply.github.com>
2024-05-11 20:31:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF'; 
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+let btn1 = document.getElementById('btn1'); 
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+let btn6 = document.getElementById('btn6');
+
+btn1.addEventListener('click', () => {
+    if (MainButton.isVisible) {
+        MainButton.hide();
+    }
+    else {
+        MainButton.setText('Button1');
+        item = "1";
+        MainButton.show();
+    }
+})
+
+btn2.addEventListener('click', () => {
+    if (MainButton.isVisible) {
+        MainButton.hide();
+    }
+    else {
+        MainButton.setText('Button2');
+        item = "2";
+        MainButton.show();
+    }
+})
+
+btn3.addEventListener('click', () => {
+    if (MainButton.isVisible) {
+        MainButton.hide();
+    }
+    else {
+        MainButton.setText('Button3');
+        item = "3";
+        MainButton.show();
+    }
+})
+
+btn4.addEventListener('click', () => {
+    if (MainButton.isVisible) {
+        MainButton.hide();
+    }
+    else {
+        MainButton.setText('Button4');
+        item = "4";
+        MainButton.show();
+    }
+})
+
+btn5.addEventListener('click', () => {
+    if (MainButton.isVisible) {
+        MainButton.hide();
+    }
+    else {
+        MainButton.setText('Button5');
+        item = "5";
+        MainButton.show();
+    }
+})
+
+btn6.addEventListener('click', () => {
+    if (MainButton.isVisible) {
+        MainButton.hide();
+    }
+    else {
+        MainButton.setText('Button6');
+        item = "6";
+        MainButton.show();
+    }
+})
+
+Telegram.WebApp.onEvent('mainButtonClicked', () => {
+    tg.sendData(item);
+})
+
+let usercard = document.getElementById('usercard');
+
+let p = document.createElement('p');
+
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
