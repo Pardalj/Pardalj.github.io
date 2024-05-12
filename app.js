41630d4ebb975e9c9de1147e2b830c3e91b20905
Pardal <141125423+Pardalj@users.noreply.github.com>
2024-05-12 15:45:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,83 +5,87 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let item = "";
+let items = [];
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+let btn = document.getElementsByClassName('btn');
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+function toggleItem(btn, itemId, price) {
+    let item = items.find(i => i.id === itemId);
+    if (!item) {
+        let newItem = {
+            id: itemId,
+            price: price
+        }
+    }
+    items.push(newItem);
+    btn.classList.toggle('added-to-cart');
+    btn.innerText = 'Delete';
+    let total = items.reduce((acc, item) => acc + item.price, 0);
+    if (total > 0) {
+        tg.MainButton.setText('Total: ' + total);
+        if (!tg.MainButton.isVisible) {
+            tg.MainButton.show();
+        } else {
+            tg.MainButton.hide();
+        }
+    } else {
+        let index = items.itemOf(item);
+        items.splice(index, 1);
+        btn.classList.remove('added-to-cart');
+        btn.innerText = 'Add';
+        let total = items.reduce((acc, item) => acc + item.price, 0);
+        if (total > 0) {
+            tg.MainButton.setText('Total: ' + total);
+            if (!tg.MainButton.isVisible) {
+                tg.MainButton.show();
+            } else {
+                tg.MainButton.hide();
+            }
+        } else {
+            tg.MainButton.hide();
+        }
+    }
+}
+
+Telegram.WebApp.onEvent('mainButtonClicked', () => {
+    let data = {
+        items: items,
+        totalPrice: calculateTotalPrice()
+    }
+    tg.sendData(JSON.stringify(data));
 });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
+function calculateTotalPrice() {
+    return items.reduce((acc, item) => acc + item.price, 0);
+}
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
+let btn6 = document.getElementById('btn6');
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
+btn1.addEventListener('click', () => {
+    toggleItem(this, "item1", 1000);
+})
 
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
+btn2.addEventListener('click', () => {
+    toggleItem(this, "item2", 2000);
+})
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
+btn3.addEventListener('click', () => {
+    toggleItem(this, "item3", 3000);
+})
 
+btn4.addEventListener('click', () => {
+    toggleItem(this, "item4", 4000);
+})
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+btn5.addEventListener('click', () => {
+    toggleItem(this, "item5", 5000);
+})
 
+btn6.addEventListener('click', () => {
+    toggleItem(this, "item6", 6000);
+})
